0643ac1af238e58f3fe32cdb3c7794c8b3544b00
Dark Knightz <faisalmoshiur@gmail.com>
2022-11-14 20:09:00 +0100

    Changes to be committed: 	new file:   NMiNE/HA/Cover.docx 	new file:   NMiNE/HA/HA06/Cover.pdf 	new file:   NMiNE/HA/HA06/NMiNE_HA06.pdf 	modified:   NMiNE/HA/HA06/Q1.py 	new file:   NMiNE/HA/HA06/Q1.py.pdf 	new file:   NMiNE/HA/HA06/Q2.docx 	new file:   NMiNE/HA/HA06/Q2.pdf     deleted:    NMiNE/HA/HA06/Q2.py 	new file:   NMiNE/HA/HA06/Q3.py 	new file:   NMiNE/HA/HA06/Q3.py.pdf 	new file:   NMiNE/HA/HA06/Q4.py 	new file:   NMiNE/HA/HA06/Q4.py.pdf 	new file:   NMiNE/HA/HA06/Q5.py 	new file:   NMiNE/HA/HA06/Q5.py.pdf 	new file:   NMiNE/HA/HA06/Q6.py 	new file:   NMiNE/HA/HA06/Q6.py.pdf 	new file:   NMiNE/HA/HA06/Q8.docx 	new file:   NMiNE/HA/HA06/Q8.pdf 	new file:   NMiNE/HA/HA06/Qs.pdf 	new file:   SETT/Debate/PUB_Comparison_of_Energy_Systens_using_lifecycle_2004_WEC.pdf 	new file:   SETT/Debate/j.adapen.2021.100058.pdf 	new file:   SETT/Debate/muralikrishna2017.pdf 	new file:   SETT/Debate/s11367-008-0008-x.pdf

--- NMiNE/HA/HA06/Q1.py
+++ NMiNE/HA/HA06/Q1.py
@@ -1,11 +1,14 @@
-import math
-import sympy
 from sympy import *
+from sympy import log as ln
+from sympy import symbols
+from sympy import diff
+from sympy import Function
+from sympy import plot
 
-t = sympy.Symbol('t')
-f = 1 / sympy.log(t)
-f_2nd_derv = sympy.diff(f, t, 2)
-f_4th_derv = sympy.diff(f, t, 4)
+t = Symbol('t')
+f = 1 / ln(t)
+f_2nd_derv = diff(f, t, 2)
+f_4th_derv = diff(f, t, 4)
 
 print(f"Function, f(t): {f}\nSecond Derv, f''(t): {f_2nd_derv}\nFourth Derv, f''''(t): {f_4th_derv}")
 
@@ -14,9 +17,7 @@ f_4 = lambdify(t, f_4th_derv)
 
 print(f"Second Derv, f''(2): {f_2(2)}\nFourth Derv, f''''(2): {f_4(2)}")
 
-'''
-f_4th_derv_num = 0
-for i in range(1, 4):
-    h = 
-    f_4th_derv_num += 
-'''
+Function('f_2nd_derv')
+Function('f_4th_derv')
+plt = plot(f, f_2nd_derv, f_4th_derv, (t, 2, 500), title="Derv. and Monotonicity", legend= True, xlabel='t', ylabel='f(t) & derv. of f(t)')
+plt.show
