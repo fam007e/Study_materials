c24f1e0612bca67d61b0eb8073afe3f02dcfbabb
Dark Knightz <faisalmoshiur@gmail.com>
2022-11-23 16:47:12 +0100

    modified:   MC_Methods/HA/HA02/LCG.py

--- MC_Methods/HA/HA02/LCG.py
+++ MC_Methods/HA/HA02/LCG.py
@@ -1,10 +1,21 @@
-import math
+import numpy as np
 
-def lcg():
-    a = 1140671485
-    c = 128201163
-    m = 2**24
-    global rand
+
+def lcg(rand):
+    a = 16807
+    c = 0
+    m = 2**31 - 1
     rand = (a*rand + c) % m
     return rand / m
 
+def pdf(x):
+    a = 0.5535
+    b = 1.0347
+    c = 1.6214
+    
+    return a * np.exp(-x / b) * np.sinh(np.sqrt(c * x))
+
+#def h_pdf(x):
+    
+u_1 = lcg(987654321)
+print(u_1)
